e7be333c8e64cf6955b2f2635713c03fe17edb1d
nazmul93-cyber <najimmycuet12@gmail.com>
2021-12-21 14:09:04 +0600

    design cont.

--- resources/views/scratch/posts.blade.php
+++ resources/views/scratch/posts.blade.php
@@ -2,18 +2,18 @@
 
     @include('scratch._posts-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            <x-post-featured-card/>
+        @if($posts->count())
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts yet. Please check back later</p>
+        @endif
 
-        <div class="lg:grid lg:grid-cols-2">
-            <x-post-card/>
-            <x-post-card/>
-        </div>
 
-        <div class="lg:grid lg:grid-cols-3">
-            <x-post-card/>
-            <x-post-card/>
-            <x-post-card/>
-        </div>
+{{--        <div class="lg:grid lg:grid-cols-3">--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--            <x-post-card/>--}}
+{{--        </div>--}}
     </main>
 
 
